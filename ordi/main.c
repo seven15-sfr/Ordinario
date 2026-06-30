4e94b141ecb0a70729aa79532951523d5b0a67d0
seven15-sfr <re23mh1@gmail.com>
2026-06-30 12:41:36 -0600

    Update main.c

--- ordi/main.c
+++ ordi/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "struct.h"
 #include "menu.h"
 #include "altas.h"
@@ -17,9 +18,37 @@ int main(void){
     do {
         op = menuPrincipal(); 
         switch (op) {
-            case 1:
-                altas(&ptrFrente, &ptrFinal);
+            case 1: {
+                // --- AGREGADO: SELECCIÓN DE CARRERA EN EL MAIN ---
+                char carreraSeleccionada[5] = "S/C"; 
+                int opCarrera = menuCarreras(); // Llamamos al menú de carreras
+                
+                switch(opCarrera) {
+                    case 1:
+                        strcpy(carreraSeleccionada, "IDS"); // Ing. en Desarrollo de Software
+                        break;
+                    case 2:
+                        strcpy(carreraSeleccionada, "IF");  // Ing. Forestal
+                        break;
+                    case 3:
+                        strcpy(carreraSeleccionada, "LAT"); // Lic. en Administración Turística
+                        break;
+                    case 4:
+                        strcpy(carreraSeleccionada, "LCA"); // Lic. en Ciencias Ambientales
+                        break;
+                    case 5:
+                        strcpy(carreraSeleccionada, "LB");  // Lic. en Biología
+                        break;
+                    default:
+                        printf("Opcion no valida. Se asignara 'S/C' (Sin Carrera).\n");
+                        strcpy(carreraSeleccionada, "S/C");
+                        break;
+                }
+                
+                // Pasamos la carrera seleccionada a la función altas
+                altas(&ptrFrente, &ptrFinal, carreraSeleccionada);
                 break;
+            }
                 
             case 2:
                 bajas(&ptrFrente, &ptrFinal);
@@ -28,56 +57,55 @@ int main(void){
             case 3:
                 menuConsultas(ptrFrente);
                 do{
-                opc = mostar(); 
-                switch (opc) {
-                    case 1: 
-                        mostrarTodaLaLista(ptrFrente); 
-                        break;
-                    case 2: 
-                        mostrarPorCarrera(ptrFrente); 
-                        break;
-                    case 3: 
-                        mostrarPorSemestre(ptrFrente); 
-                        break;
-                    case 4: 
-                        mostrarPorCarreraSemestre(ptrFrente); 
-                        break;
-                    case 5: 
-                        buscarPorNombre(ptrFrente); 
-                        break;
-                    case 6:    
-                        buscarPorMatricula(ptrFrente); 
-                        break;
-                    case 7:
-                        printf("Regresando al menu principal \n");
-                        break;
-                    default: 
-                        printf("Opcion de consulta no valida \n"); 
-                        break;
-                } 
-            }while(opc != 7);
-            break;
+                    opc = mostar(); 
+                    switch (opc) {
+                        case 1: 
+                            mostrarTodaLaLista(ptrFrente); 
+                            break;
+                        case 2: 
+                            mostrarPorCarrera(ptrFrente); 
+                            break;
+                        case 3: 
+                            mostrarPorSemestre(ptrFrente); 
+                            break;
+                        case 4: 
+                            mostrarPorCarreraSemestre(ptrFrente); 
+                            break;
+                        case 5: 
+                            buscarPorNombre(ptrFrente); 
+                            break;
+                        case 6:    
+                            buscarPorMatricula(ptrFrente); 
+                            break;
+                        case 7:
+                            printf("Regresando al menu principal \n");
+                            break;
+                        default: 
+                            printf("Opcion de consulta no valida \n"); 
+                            break;
+                    } 
+                } while(opc != 7);
+                break;
 
             case 4:
                 ejecutarSubmenu(ptrFrente);
-                  do {
-                        opSub = submenu(); 
-                        switch (opSub) {
-                            case 1:
-                                modificarDatosAlumno(ptrFrente);
-                                break;
-                            case 2:
-                                registrarCalificaciones(ptrFrente);
-                                break;
-                            case 3:
-                                printf("Regresando al menu principal \n");
-                                break;
-                            default:
-                                printf("Opcion no valida \n");
-                                break;
-                        }
-                    } while (opSub != 3); // Se repite hasta que elijan la opción 3 
-                
+                do {
+                    opSub = submenu(); 
+                    switch (opSub) {
+                        case 1:
+                            modificarDatosAlumno(ptrFrente);
+                            break;
+                        case 2:
+                            registrarCalificaciones(ptrFrente);
+                            break;
+                        case 3:
+                            printf("Regresando al menu principal \n");
+                            break;
+                        default:
+                            printf("Opcion no valida \n");
+                            break;
+                    }
+                } while (opSub != 3); 
                 break;
 
             case 5:
