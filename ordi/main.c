7011cbf57d392d0748a35a09347e3271e23b2240
seven15-sfr <re23mh1@gmail.com>
2026-06-30 08:13:26 -0600

    Enhance consultas functionality with submenu options
    Refactor consultas handling to use menu and sub-options.

--- ordi/main.c
+++ ordi/main.c
@@ -5,10 +5,11 @@
 #include "altas.h"
 #include "bajas.h"
 #include "consultas.h"
-#include "submenu.h"   
+#include "submenu.h"
 
 int main(void){
     int op;
+    int opc; 
     struct Persona *ptrFrente = NULL;
     struct Persona *ptrFinal = NULL;
 
@@ -22,8 +23,38 @@ int main(void){
                 bajas(&ptrFrente, &ptrFinal);
                 break;
             case 3:
-                ejecutarConsultas(ptrFrente); 
-                break;
+                menuConsultas(ptrFrente);
+                do{
+                opc = mostar(); 
+                switch (opc) {
+                    case 1: 
+                        mostrarTodaLaLista(ptrFrente); 
+                        break;
+                    case 2: 
+                        mostrarPorCarrera(ptrFrente); 
+                        break;
+                    case 3: 
+                        mostrarPorSemestre(ptrFrente); 
+                        break;
+                    case 4: 
+                        mostrarPorCarreraSemestre(ptrFrente); 
+                        break;
+                    case 5: 
+                        buscarPorNombre(ptrFrente); 
+                        break;
+                    case 6:    
+                        buscarPorMatricula(ptrFrente); 
+                        break;
+                    case 7:
+                        printf("Regresando al menu principal \n");
+                        break;
+                    default: 
+                        printf("Opcion de consulta no valida \n"); 
+                        break;
+                } 
+            }while(opc != 7);
+            break;
+
             case 4:
                 ejecutarSubmenu(ptrFrente);
                 break;
@@ -34,8 +65,8 @@ int main(void){
             default:
                 printf("Seleccione una opcion valida \n");
                 break;
-        }
-    } while (op != 5); 
+        } 
+    } while (op != 5);
       
     return 0;
 }
