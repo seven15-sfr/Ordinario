6d1072bccdbef22a99106f2fc4835026c2eb8c49
seven15-sfr <re23mh1@gmail.com>
2026-06-29 23:47:46 -0600

    Fix header guard formatting in altas.h

--- ordi/altas.h
+++ ordi/altas.h
@@ -3,7 +3,6 @@
 
 #include "struct.h"
 
-/* Homologado a minúsculas y recibiendo ambos punteros para la Cola (FIFO) */
 int altas(struct Persona **ptrFrente, struct Persona **ptrFinal);
 struct Persona* nuevaPersona(void);
 struct Alumno* nuevoAlumno(void);
